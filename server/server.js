5cd3df6b6766bdf1fedad66171913af402240fe3
Jacob Albright <jacob.albright23@gmail.com>
2018-05-09 14:23:12 -0500

    propublica routes and service folders and files set up server side

--- server/server.js
+++ server/server.js
@@ -4,14 +4,33 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 // Route includes
-
-
+const { 
+  membersRouter, 
+  billsRouter, 
+  votesRouter,
+  statementsRouter,
+  committeesRouter  
+} = require('./routes/propublica');
 
 const app = express();
 
+// Use morgan and body-parser middleware
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+// Serve static files
+app.use(express.static('build'));
 
 // Routes //
+app.use('/congress/members', membersRouter);
+app.use('/congress/bills', billsRouter);
+app.use('/congress/votes', votesRouter);
+app.use('/congress/statements', statementsRouter);
+app.use('/congress/committees', committeesRouter);
 
+const port = process.env.PORT || 8081
+app.listen(port, () => console.log(`Server ready on port ${port}`))
+
+app.get('/api/test', (req, res) => {
+  res.send({success: 'YES!'});
+});
