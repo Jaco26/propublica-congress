4149ca861c387005c37dc26bf4723ab76a3c3cae
Jacob Albright <jacob.albright23@gmail.com>
2018-05-21 12:35:28 -0500

    cleaned up markdown in Role

--- src/components/Members/Member/Role.vue
+++ src/components/Members/Member/Role.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container :class="{active: open}">
-    <!-- <v-layout  column :class="{active: open}"> -->
       <v-layout >
         <v-flex>
           <h2>{{role.title}} <small>{{role.state}} {{role.party}} </small> </h2>
@@ -33,43 +32,7 @@
         </v-flex>
       </v-layout>
       <v-divider></v-divider>
-        
-    <!-- </v-layout> -->
   </v-container>
-
-  <!-- <v-container>
-    <v-layout  column :class="{active: open}">
-      <v-layout>
-        <v-flex>
-          <h2>{{role.title}} <small>{{role.state}} {{role.party}} </small> </h2>
-          {{dates}}
-        </v-flex>
-        <v-flex>
-          <v-btn :color="open ? btnClose: btnOpen" flat icon  @click="toggle"> 
-            <v-icon  v-if="!open">add</v-icon> 
-            <v-icon v-if="open">remove</v-icon> 
-          </v-btn>
-        </v-flex>
-      </v-layout>
-        <v-flex v-show="open" class="list-complete-item">
-          <small><b>Bills Sponsored:</b></small> {{role.bills_sponsored}} <br>
-          <small><b>Bills Cosponsored:</b></small> {{role.bills_cosponsored}} <br>
-          <small><b>Missed Votes Pct:</b></small> {{role.missed_votes_pct}}% <br>
-          <small><b>Votes With Party:</b></small> {{role.votes_with_party_pct}}% <br>
-          <div v-if="role.district">
-            <small><b>District:</b></small> {{role.district}}
-          </div>
-          <ul v-if="committees">
-            <committee 
-              v-for="(committee, j) in committees" 
-              :key="j"
-              :committee="committee" 
-            />
-          </ul>
-        </v-flex>
-    </v-layout>
-  </v-container> -->
-  
 </template>
 
 <script>
