8bd02f369f1be4ed2ce32ea59ee7832e69b7992e
Jacob Albright <jacob.albright23@gmail.com>
2018-05-31 12:16:30 -0500

    the navigation drawer now contains conditionally rendered nest v-list-groups

--- src/components/Shared/NavDrawer.vue
+++ src/components/Shared/NavDrawer.vue
@@ -8,17 +8,14 @@
       dark 
       class="primary"
     >
-      <v-toolbar-side-icon 
-        @click="drawer = !drawer"
-        class="hidden-md-and-up"
-      ></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer;">KnowThyCongress</router-link> 
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
 
-    <v-navigation-drawer 
+    <v-navigation-drawer
       clipped 
       app 
       v-model="drawer" 
@@ -27,48 +24,72 @@
       class="elevation-3"
       id="navigation-drawer"
     >
-
-      <v-list dense>
-        <v-list-tile router :to="home.path">
-          <v-list-tile-content>
-            <v-list-tile-title class="title">
-              {{home.title}}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-       
-        <v-list-group 
-          sub-group
-          no-action
-          v-for="item in proPubItems"
+      <v-list dense>  
+        <template v-for="item in navDrawerItems">
+          <!-- If item does NOT have children (v-if) -->
+          <v-list-tile 
+          v-if="!item.children" 
           :key="item.title"
-          v-model="item.active"
-        >
-          <v-list-tile slot="activator">
+          router
+          :to="item.path"
+          >
             <v-list-tile-content>
               <v-list-tile-title class="title">
                 {{item.title}}
               </v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile 
-            v-for="child in item.children" 
-            :key="child.title"
-            router
-            :to="child.path"
-          >
-            <v-list-tile-content>
-              <v-list-tile-title class="subheading">{{child.title}}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-group>
+
+          <!-- If item DOES have children (v-else) -->
+          <v-list-group v-else v-model="item.active" :key="item.title">
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title class="title">
+                  {{item.title}}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <!-- if child does NOT have children of its own (v-if)
+            render list-tile FOR EACH of item's children (v-for) -->
+            <v-list-tile v-if="!child.children" v-for="child in item.children" :key="child.title">
+              <v-list-tile-content>
+                <v-list-tile-title class="subheading">
+                  {{child.title}}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <!-- if child DOES have children of its own (v-else)
+            render a list group for those children -->
+            <v-list-group v-else v-model="child.active" :key="child.title" sub-group>
+              <v-list-tile slot="activator">
+                <v-list-tile-content>
+                  <v-list-tile-title class=" app-nav-header">
+                    {{child.title}}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <!-- render a list-tile FOR EACH grandChild of child's children -->
+              <v-list-tile 
+              v-for="grandChild in child.children" 
+              :key="grandChild.title"
+              router
+              :to="grandChild.path"
+              >
+               <v-list-tile-action>
+                    <v-icon></v-icon>
+                  </v-list-tile-action>
+                <v-list-tile-content >
+                  <v-list-tile-title class="app-nav-link">
+                    {{grandChild.title}}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+          </v-list-group>
+        </template>
       </v-list>
     </v-navigation-drawer>
   </div>
-
-
-  
 </template>
 
 <script>
@@ -77,47 +98,57 @@ export default {
   data: () => ({
       drawer: null,
       home: { title: 'Home', path: '/' },
-      proPubItems: [
-        { 
-          active: false,
-          title: 'Members', 
-          children: [
-            { title: 'Senate', path: '/members/senate' },
-            { title: 'House', path: '/members/house' },
-            { title: 'New Members', path: '/members/new' },
-            { title: 'Members Leaving Office', path: '/members/leaving' },
-          ]
+      navDrawerItems: [
+        {
+          title: 'Home', 
+          path: '/'
         },
         {
+          title: 'Explore',
           active: false,
-          title: 'Votes',
           children: [
-            { title: 'Recent Explanations', path: '/votes/explanations' },
-          ]
-        },
-        {
-          active: false,
-          title: 'Bills',
-          children: [
-            { title: 'Search Bills', path: '/bills/search' },
-            { title: 'Recent Bills', path: '/bills/recent' },
-            { title: 'Upcoming Bills', path: '/bills/upcoming' },
-          ]
-        },
-        {
-          active: false,
-          title: 'Statements', 
-          children: [
+            { 
+              active: false,
+              title: 'Members', 
+              children: [
+                { title: 'Senate', path: '/members/senate' },
+                { title: 'House', path: '/members/house' },
+                { title: 'New Members', path: '/members/new' },
+                { title: 'Members Leaving Office', path: '/members/leaving' },
+              ]
+            },
+            {
+              active: false,
+              title: 'Votes',
+              children: [
+                { title: 'Recent Explanations', path: '/votes/explanations' },
+              ]
+            },
+            {
+              active: false,
+              title: 'Bills',
+              children: [
+                { title: 'Search Bills', path: '/bills/search' },
+                { title: 'Recent Bills', path: '/bills/recent' },
+                { title: 'Upcoming Bills', path: '/bills/upcoming' },
+              ]
+            },
+            {
+              active: false,
+              title: 'Statements', 
+              children: [
 
-          ]
-        },
-        {
-          active: false,
-          title: 'Committees',
-          children: [
+              ]
+            },
+            {
+              active: false,
+              title: 'Committees',
+              children: [
 
+              ]
+            }
           ]
-        }
+        } 
       ]
     }),
 }
@@ -128,5 +159,24 @@ export default {
 #navigation-drawer {
   background-color: #f8f8f8;
 }
+
+.app-nav-header {
+  font-size: 18px;
+  font-weight: 100;
+}
+
+.app-nav-link {
+  font-size: 16px;
+  font-weight: 600;
+  color: #6e6e6e
+}
+
+.app-nav-link:hover {
+  font-size: 16px;
+  font-weight: 600;
+  color: #1f1f1f;
+}
+
+
 
 </style>
